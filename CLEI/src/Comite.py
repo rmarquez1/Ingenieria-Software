5c29134cef706f671aa681f6d80ce8b4d07822ba
daralion <gabrielgedler@gmail.com>
2013-12-09 10:11:49 -0430

    Update Comite.py

--- CLEI/src/Comite.py
+++ CLEI/src/Comite.py
@@ -43,13 +43,6 @@ class Comite():
         self.esPresidente = esPresidente
 
 
-class Test():
-    # Constructor comite
-    def __init__(self, correo= None, nombre=None, apellido=None):
-        self.t1 = correo
-        self.t1 = nombre
-        self.t1 = apellido
-        self.t1 = False
 
 
         

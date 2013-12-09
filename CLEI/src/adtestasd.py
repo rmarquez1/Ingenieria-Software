b8298e271acda340913c44516d598c43350d97b1
daralion <gabrielgedler@gmail.com>
2013-12-09 17:51:06 -0430

    Delete adtestasd.py

--- CLEI/src/adtestasd.py
+++
@@ -1,7 +0,0 @@
-class Test():
-    # Constructor comite
-    def __init__(self, correo= None, nombre=None, apellido=None):
-        self.t1 = correo
-        self.t1 = nombre
-        self.t1 = apellido
-        self.t1 = False
